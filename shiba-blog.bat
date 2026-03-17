0de076fae551b2907e560e304578a3ac4772d4be
Your Name <you@example.com>
2026-03-17 21:26:37 +0900

    lenovo

--- shiba-blog.bat
+++ shiba-blog.bat
@@ -1,4 +1,4 @@
 set BLOGDIR=shibatsuri
 cd %USERPROFILE%\blog\%BLOGDIR%
-start %USERPROFILE%\AppData\Local\Programs\sakura242\sakura.exe -M=C:\Users\who\AppData\Local\Programs\sakura242\sakura-items\macros\hugo_list.js
-hugo server -D -O --navigateToChanged
+start %USERPROFILE%\AppData\Local\Programs\sakura242\sakura.exe -M=%USERPROFILE%\AppData\Local\Programs\sakura242\sakura-items\macros\hugo_list.js
+cmd /k hugo server -D -O --navigateToChanged
