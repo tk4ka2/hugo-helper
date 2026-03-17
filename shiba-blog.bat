9b1251a7d6f3a86c1038bb037ba93f73ee417c9f
tk4ka2 <tk4ka2@gmail.com>
2026-03-17 20:31:38 +0900

    chore(hugo): hugo server をフォアグラウンドで実行する

--- shiba-blog.bat
+++ shiba-blog.bat
@@ -1,5 +1,4 @@
 set BLOGDIR=shibatsuri
 cd %USERPROFILE%\blog\%BLOGDIR%
-start hugo server -D -O --navigateToChanged
-%USERPROFILE%\AppData\Local\Programs\sakura242\sakura.exe -M=C:\Users\who\AppData\Local\Programs\sakura242\sakura-items\macros\hugo_list.js
-
+start %USERPROFILE%\AppData\Local\Programs\sakura242\sakura.exe -M=C:\Users\who\AppData\Local\Programs\sakura242\sakura-items\macros\hugo_list.js
+hugo server -D -O --navigateToChanged
